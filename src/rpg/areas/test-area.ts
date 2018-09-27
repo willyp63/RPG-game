6b4e4de26be4ae97115dc1ad6ff726d3b317bd7d
Wil Pirino <wpirino1@gmail.com>
2018-09-27 16:14:35 -0600

    less slimes

--- src/rpg/areas/test-area.ts
+++ src/rpg/areas/test-area.ts
@@ -55,7 +55,7 @@ export default class TestArea extends PIXISystem {
     this.followEntity(warrior);
 
     // Slimes
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 4; i++) {
       const dx = Math.random() * 360;
       this.addEntity(new Slime(new Vector(440 + dx, 200)));
     }
