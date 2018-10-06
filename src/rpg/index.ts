15a330d926cc2df36fe5efde7705f419c18a5af4
Wil Pirino <wpirino1@gmail.com>
2018-10-06 15:21:55 -0600

    foo

--- src/rpg/index.ts
+++ src/rpg/index.ts
@@ -1,4 +1,4 @@
 import RPGSystem from "./rpg-system";
 import Vector from "../engine/core/vector";
 
-new RPGSystem('public/areas/ruins-4.json', new Vector(422, 241));
+new RPGSystem('public/areas/ruins-1.json', new Vector(240, 240));
